6308eeaa7dd9989b78cd29715c208fd35485d1db
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-06 22:03:01 +0530

    added new code

--- Patterns/halfpyramid/halfpyra.cpp
+++ Patterns/halfpyramid/halfpyra.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     int rows;
-    cout<<"Enter the rows:"<<endl;
+    cout<<"Enter the no.of rows:"<<endl;
     cin>>rows;
     for(int i=rows;i>=1;i--){
         for(int j=1;j<=i;j++){
