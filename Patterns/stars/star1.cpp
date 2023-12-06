9b4af2be7a9de1c1cc4119954abc90735797519b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-06 22:04:19 +0530

    added new code

--- Patterns/stars/star1.cpp
+++ Patterns/stars/star1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int rows;
-    cout<<"Enter the rows:"<<endl;
+    cout<<"Enter the no.of rows:"<<endl;
     cin>>rows;
     for(int i=1;i<=rows;i++){
         for(int j=1;j<=4;j++){
