f27a956f30496c1cceb808661d2f0e01f6764245
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-06 22:02:39 +0530

    added new code

--- Patterns/floydstriangle/floyd.cpp
+++ Patterns/floydstriangle/floyd.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int n;
     int num=1;
-    cout<<"Enter the rows:"<<endl;
+    cout<<"Enter the no.of rows:"<<endl;
     cin>>n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
