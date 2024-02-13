21ddeacf52bfe73a9e7ab34bee7f9b7e5d851c6a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-13 15:39:22 +0530

    code added

---
+++ Arrays/maxofarray/arraymax.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the size of array:"<<endl;
+    cin>>n;
+    int array[n];
+    cout<<"Enter the elements of array:"<<endl;
+    
+}
