f3e993e260fc0066cabb072a36628396ee624e84
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-13 15:43:50 +0530

    added new code

--- Arrays/maxofarray/arraymax.cpp
+++ Arrays/maxofarray/arraymax.cpp
@@ -7,5 +7,17 @@ int main(){
     cin>>n;
     int array[n];
     cout<<"Enter the elements of array:"<<endl;
-    
+    for(int i=0;i<n;i++){
+        cin>>array[i];
+    }
+
+    int mx=INT_MIN;
+
+    for(int i=0;i<n;i++){
+        mx=max(mx,array[i]);
+        cout<<mx<<" ";
+    }
+    cout<<endl;
+    cout<<"Maximum of the array is: "<<mx<<endl;
+    return 0;
 }
