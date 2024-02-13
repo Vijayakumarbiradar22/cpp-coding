a8519a6116ab9bf6a2f5226b067b0fdab6a1616e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-13 15:59:54 +0530

    added new code

--- Arrays/sumofsubarray/sumofsub.cpp
+++ Arrays/sumofsubarray/sumofsub.cpp
@@ -10,5 +10,15 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>array[i];
     }
-    
+
+    int curr=0;
+
+    for(int i=0;i<n;i++){
+        curr=0;
+        for(int j=i;j<n;j++){
+            curr+=array[j];
+            cout<<curr<<endl;
+        }
+    }
+    return 0;
 }
