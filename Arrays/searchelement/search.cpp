3280a113093e869d6577f7a49754b3654ea2f32d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-13 11:14:06 +0530

    code changed

--- Arrays/searchelement/search.cpp
+++ Arrays/searchelement/search.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 int linearsearch(int array[],int n,int key){
-    
+    for(int i=0;i<n;i++){
+        if(array[i]==key){
+            return i;
+        }
+    }
+    return -1;
 }
 
 int main(){
