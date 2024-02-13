9930277ad4b0f9cb621e6a92935a532086ff5758
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-13 19:34:08 +0530

    added new code

--- Arrays/spiralorder/spiral.cpp
+++ Arrays/spiralorder/spiral.cpp
@@ -5,5 +5,14 @@ int main(){
     int r,c;
     cout<<"Enter the number of rows and columns:"<<endl;
     cin>>r>>c;
+    int mat[r][c];
+
+    cout<<"Enter the elements of matrix:"<<endl;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>mat[i][j];
+        }
+    }
+
     
 }
