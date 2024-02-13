dbe033082314ba559346f2400a3200665f83d1f1
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-13 11:41:28 +0530

    added code

--- Arrays/binarysearch/binary.cpp
+++ Arrays/binarysearch/binary.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
 
+
+
 int main(){
-    
+    int n;
+    cout<<"Enter the size of array:"<<endl;
+    cin>>n;
+    int array[n];
+    cout<<"Enter the elements of array:"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>array[i];
+    }
+    int key;
+    cout<<"Enter the key to be searched"<<endl;
+    cin>>key;
+
+    cout<<binarysearch(array,n,key);
 }
