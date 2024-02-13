fa282518a8d983e682438628b946480fcb578bcc
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-13 12:37:34 +0530

    added code

--- Arrays/Sorting/insertion/insert.cpp
+++ Arrays/Sorting/insertion/insert.cpp
@@ -15,6 +15,14 @@ int main(){
     for(int i=1;i<n;i++){
         int current=array[i];
         int j=i-1;
-        
+        while(array[j]>current && j>=0){
+            array[j+1]=array[j];
+            j--;
+        }
+        array[j+1]=current;
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<array[i]<<" ";
     }
 }
