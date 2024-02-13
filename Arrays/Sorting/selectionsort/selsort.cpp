bc22bf9ece505e129de9fea9f354679b6d8205c7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-13 12:05:01 +0530

    added code

--- Arrays/Sorting/selectionsort/selsort.cpp
+++ Arrays/Sorting/selectionsort/selsort.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cout<<"Enter the size of array:"<<endl;
+    cin>>n;
+    int array[n];
+    cout<<"Enter the elements of array:"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>array[i];
+    }
+
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(array[j]<array[i]){
+                int temp=array[j];
+                array[j]=array[i];
+                array[i]=temp;
+            }
+        }
+    }
+
+    cout<<"Sorted array is"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<array[i]<<" ";
+    }
 }
