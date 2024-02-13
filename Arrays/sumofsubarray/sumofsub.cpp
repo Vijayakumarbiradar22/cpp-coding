a751e2df559178a5e16ce04217d6e89a561a9838
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-13 15:57:13 +0530

    added new code

---
+++ Arrays/sumofsubarray/sumofsub.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the size of array:"<<endl;
+    cin>>n;
+    int array[n];
+    cout<<"Enter the elements of array:"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>array[i];
+    }
+    
+}
