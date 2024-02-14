119a7c1a44b6e958ea1f91d31d4530dfcdbb609a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-14 19:00:32 +0530

    added new code

--- Pointers/basic.cpp
+++ Pointers/basic.cpp
@@ -10,10 +10,10 @@ int main(){
     cout<<aptr<<endl; //next address
     
     int arr[]={10,20,30};
-    cout<<*arr<<endl;
+    cout<<*arr<<endl;  //prints 1st element of array
 
     int *ptr=arr;
-    for(int i=0;i<3;i++){
+    for(int i=0;i<3;i++){  //prints all the array elements
         cout<<*ptr<<endl;
         ptr++;
     }
