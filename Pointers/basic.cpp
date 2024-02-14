94fa26093293db063366261428d12ab96155048c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-14 18:59:00 +0530

    added new code

--- Pointers/basic.cpp
+++ Pointers/basic.cpp
@@ -3,8 +3,19 @@ using namespace std;
 
 int main(){
     int a=10;
-    int  *aptr;
-    aptr=&a;
-    cout<<aptr<<endl; //address of a
+    int *aptr=&a;
     cout<<*aptr<<endl;
+    cout<<aptr<<endl; //address of a
+    aptr++;
+    cout<<aptr<<endl;
+    
+    int arr[]={10,20,30};
+    cout<<*arr<<endl;
+
+    int *ptr=arr;
+    for(int i=0;i<3;i++){
+        cout<<*ptr<<endl;
+        ptr++;
+    }
+
 }
