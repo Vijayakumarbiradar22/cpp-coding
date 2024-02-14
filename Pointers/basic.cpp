7aac569ccd59823f04d42425bd85a8dc18b87251
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-14 18:59:54 +0530

    added new code

--- Pointers/basic.cpp
+++ Pointers/basic.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main(){
     int a=10;
     int *aptr=&a;
-    cout<<*aptr<<endl;
+    cout<<*aptr<<endl; //prints a value i.e 10
     cout<<aptr<<endl; //address of a
     aptr++;
-    cout<<aptr<<endl;
+    cout<<aptr<<endl; //next address
     
     int arr[]={10,20,30};
     cout<<*arr<<endl;
@@ -17,5 +17,7 @@ int main(){
         cout<<*ptr<<endl;
         ptr++;
     }
+
+
 
 }
