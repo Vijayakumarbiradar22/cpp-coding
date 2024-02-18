3c7d15d7fad09535bb4a8d91ee7c7fbac4492763
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-18 16:14:09 +0530

    added new code

--- Strings/maxfrequence/freq.cpp
+++ Strings/maxfrequence/freq.cpp
@@ -15,5 +15,13 @@ int main(){
         freq[s[i]-'a']++;
     }
 
-    
+    char ans='a';
+    int maxF=0;
+
+    for(int i=0;i<26;i++){
+        if(freq[i]>maxF){
+            maxF=freq[i];
+            ans=i+'a';
+        }
+    }
 }
